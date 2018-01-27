5b18eac27f0d4fd8c1514201db53c7d0a16362db
Daniil-Sokolov <daniil.sok2017@yandex.ru>
2018-01-27 22:27:15 +0300

    jekyll1

---
+++ assets/common.js
@@ -0,0 +1,24 @@
+$(function(){
+      
+      // pjax
+      $(document).pjax('a', '#pjax-container', {fragment: '#pjax-container'});
+    })
+
+
+
+
+$(document).on('ready pjax:end', function(event) {
+
+
+
+$(document).ready(function() { 
+
+   $.getScript('/assets/ytplayer/jquery.mb.ytplayer.min.js');
+
+   $.getScript('/assets/theme/js/script.js');
+
+})
+
+})
+
+
